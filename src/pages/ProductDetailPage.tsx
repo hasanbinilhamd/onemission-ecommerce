9d6e968f78c9789b942b761e3a7e6065ef8e1d1e
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2026-08-14 15:06:20 +0700

    fix: padding detail page

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -468,7 +468,7 @@ function ProductDetailContent() {
           backdropFilter: 'blur(12px)',
           WebkitBackdropFilter: 'blur(12px)',
           borderBottom: '1px solid #F3F4F6',
-          padding: '0 24px',
+          padding: '0 14px',
           height: '80px',
           display: 'flex',
           alignItems: 'center',
